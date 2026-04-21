6900d79c60b5078430719c700199a298857e8402
Tisjn <tinshinpro.com>
2026-04-21 21:25:19 +0700

    update .env

--- services/auth_service/src/main/java/iuh/fit/auth_service/security/JwtUtil.java
+++ services/auth_service/src/main/java/iuh/fit/auth_service/security/JwtUtil.java
@@ -2,6 +2,7 @@ package iuh.fit.auth_service.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -10,14 +11,23 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final SecretKey SECRET = Keys.hmacShaKeyFor("mysecretkeymysecretkeymysecretkey".getBytes());
+    private final SecretKey secretKey;
+    private final long jwtExpireMs;
+
+    public JwtUtil(
+            @Value("${JWT_SECRET}") String jwtSecret,
+            @Value("${JWT_EXPIRE_MS}") long jwtExpireMs
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        this.jwtExpireMs = jwtExpireMs;
+    }
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .signWith(SECRET)
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpireMs))
+                .signWith(secretKey)
                 .compact();
     }
 }
